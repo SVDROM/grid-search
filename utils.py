00ca231c90a5bc855f67b633d1414bb6514cc94f
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2026-03-06 22:13:25 +0000

    add compute_climatology function to utils module

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 import xarray as xr
 import numpy as np
 
@@ -31,3 +32,45 @@ def compute_rmse(
     rmse = rmse.clip(min=0)
     rmse = xr.ufuncs.sqrt(rmse)
     return rmse.compute()
+
+
+def compute_climatology(
+    data: xr.DataArray,
+    year: int,
+    months: list[int],
+) -> xr.DataArray:
+    """Given observed data, compute a climatological forecast over
+    the specified year and months.
+
+    Examples
+    --------
+    Given observed data for 2016-2019, compute the climatological
+    forecast for Jan and Feb 2020:
+
+    >>> climatology = compute_climatology(
+            ground_truth.sel(time=slice("2016-01-01", "2019-12-31")),
+            year=2020,
+            months=[1, 2],
+        )
+    """
+    months = sorted(months)
+    climatology = data.sel(time=data.time.dt.month.isin(months))
+    climatology = climatology.compute()
+    climatology = climatology.sel(
+        time=~((climatology.time.dt.month == 2) & (climatology.time.dt.day == 29))
+    )  # drop 29th Feb for consistency
+    climatology = climatology.groupby(["time.dayofyear", "time.hour"]).mean()
+    hours = climatology.hour.values
+    days = climatology.dayofyear.values
+    t = []
+    for d in days:
+        for h in hours:
+            dt = timedelta(days=int(d - 1), hours=int(h))
+            t.append(np.timedelta64(dt, "h"))
+    t = np.array(t)
+    month = months[0]
+    month = f"0{month}" if month < 10 else str(month)
+    t = np.datetime64(f"{year}-{month}-01T00:00") + t
+    climatology = climatology.stack(time=("dayofyear", "hour"))
+    climatology = climatology.drop_vars(["time", "dayofyear", "hour"])
+    return climatology.assign_coords(time=t)
